51071b9b707f43f84f3a03d8bc824ce2c9a608cd
arsummers <aliyasummers1@gmail.com>
2021-04-03 11:43:39 -0700

    getting good IP data

--- weather_backend/location/views.py
+++ weather_backend/location/views.py
@@ -7,7 +7,7 @@ def location(request):
     ip_address = os.environ.get('IP_ADDRESS')
     ip_api_key = os.environ.get('IPSTACK_API_KEY')
 
-    response = requests.get(f'http://api.ipstack.com/{ip_address}?access_key={ip_api_key}')
+    response = requests.get(f'http://api.ipstack.com/check?access_key={ip_api_key}')
     location_data = response.json()
     return render(request, 'location.html', {
         'ip' : location_data['ip'],
