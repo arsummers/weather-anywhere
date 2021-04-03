3a59b2f95dd4094363290c5bc9f0ea2e809a82a3
arsummers <aliyasummers1@gmail.com>
2021-04-03 11:34:34 -0700

    got templates working, will have to move to react

--- weather_backend/location/views.py
+++ weather_backend/location/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+import requests
+import os
 
-# Create your views here.
+def location(request):
+    #but how to make the IP address part dynamic??
+    ip_address = os.environ.get('IP_ADDRESS')
+    ip_api_key = os.environ.get('IPSTACK_API_KEY')
+
+    response = requests.get(f'http://api.ipstack.com/{ip_address}?access_key={ip_api_key}')
+    location_data = response.json()
+    return render(request, 'location.html', {
+        'ip' : location_data['ip'],
+        'country' : location_data['country_name']
+    })
--- weather_backend/weather_backend/settings.py
+++ weather_backend/weather_backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'location',
+    'weather'
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'weather_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
